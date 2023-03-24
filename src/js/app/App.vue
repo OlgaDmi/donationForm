a964d1416cd21206312695a20eabd15814c117a1
Olga Dmitrieva <OlgaDmi1992@mail.ru>
2023-03-24 17:43:32 +0300

    dev branch - App components were created, desctop styles were created, mobile version before 600px

---
+++ src/js/app/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app-inner" class="app-inner">
+	  <Header/>
+	  <div class="content">
+		  <div class="content__inner_left">
+			  <DonationsBlock/>
+			  <Transactions/>
+		  </div>
+		  <div class="content__inner_right">
+			  <MenuRight/>
+		  </div>
+	  </div>
+  </div>
+</template>
+
+<script>
+import Transactions from './components/Transactions.vue'
+import DonationsBlock from './components/DonationsBlock.vue'
+import Header from './components/Header.vue'
+import MenuRight from './components/MenuRight.vue'
+export default {
+  name: 'App',
+	components: {
+		Transactions,
+		DonationsBlock,
+		Header,
+		MenuRight,
+	},
+  // data() {
+  //   return {
+  //     // user: 'Василий Пупкин'
+  //   }
+  // }
+}
+</script>
